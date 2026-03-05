de1dc2fd1b9f4f52f20cf70f61de92093f7e05bf
Nikhil0264 <nikhilpadul95@gmail.com>
2026-03-05 16:58:17 +0530

    making way to uploading images currently using "uploadthing" .cloudinary has to setup in future

---
+++ frontend/app/(admin)/posts/[id]/page.tsx
@@ -0,0 +1,7 @@
+export default function PostPage(){
+    return (
+        <div>
+            <h1>single post page</h1>
+        </div>
+    )
+}
---
+++ frontend/app/(admin)/posts/page.tsx
@@ -0,0 +1,7 @@
+export default function PostPage(){
+    return (
+        <div>
+            <h1>posts page</h1>
+        </div>
+    )
+}
